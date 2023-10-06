8c923cf88d8e6cb838154866b353e4496fa4f09b
Carlos Moraes <carloshrmoraes@gmail.com>
2023-10-06 10:57:01 -0300

    day p1 complete

--- Day4.cpp
+++ Day4.cpp
@@ -4,10 +4,28 @@
 #include <vector>
 #include <set>
 #include <stack>
+#include <charconv>
+#include <utility>
 
 class Day4
 {
+private:
 	std::vector<std::string> lines{ };
+
+	std::pair<int, int> parseRange(std::string_view part)
+	{
+		size_t div = part.find("-");
+
+		std::string_view const fp_start_str{ part.substr(0, div) };
+		int fp_start_num{ 0 };
+		std::from_chars(fp_start_str.data(), fp_start_str.data() + fp_start_str.size(), fp_start_num);
+
+		std::string_view const fp_end_str{ part.substr(div + 1, part.size() - div) };
+		int fp_end_num{ 0 };
+		std::from_chars(fp_end_str.data(), fp_end_str.data() + fp_end_str.size(), fp_end_num);
+
+		return std::make_pair(fp_start_num, fp_end_num);
+	}
 
 public:
 	Day4()
@@ -20,25 +38,33 @@ public:
 
 	int partOne()
 	{
+		int overlap{ 0 };
 		for (std::string_view section : lines)
 		{
-			size_t fp_div = 0;
-			size_t sp_div = 0;
+			size_t division = section.find(",");
+			std::string_view const first_part{ section.substr(0, division) };
+			std::string_view const secnd_part{ section.substr(division + 1, section.size() - division) };
 
-			for (size_t i = 0; i < section.size(); i++)
+			auto first_range = parseRange(first_part);
+			auto second_range = parseRange(secnd_part);
+
+			if (first_range.first == second_range.first)
+				overlap++;
+			else
 			{
-				if (section[i] == '-')
+				if (first_range.first < second_range.first)
 				{
-					if (fp_div == 0)
-						fp_div = i;
-					else
-						sp_div = i;
-				}	
+					if (first_range.second >= second_range.second)
+						overlap++;
+				}
+				else
+				{
+					if (first_range.second <= second_range.second)
+						overlap++;
+				}
 			}
-
-			int fp_start{ std::stoi(section, )};
 		}
-		return 0;
+		return overlap;
 	}
 
 	double partTwo()
