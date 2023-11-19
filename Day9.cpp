7d7e1905ae6c28e470bdf6a12c61e1e6c933c7ea
Carlos Moraes <carloshrmoraes@gmail.com>
2023-11-19 09:26:20 -0300

    day9 pt1

--- Day9.cpp
+++ Day9.cpp
@@ -1,8 +1,20 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <map>
 #include <charconv>
 #include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+
+const std::map<char, std::tuple<int, int>> dir_offset{
+	{'D', {	 0,  1}},
+	{'U', {	 0,	-1}},
+	{'L', {	-1,  0}},
+	{'R', {	 1,	 0}},
+};
 
 class Day9
 {
@@ -26,7 +38,11 @@ public:
 
 	int partOne()
 	{
-		auto rope_head = new Rope();
+		auto my_rope = new Rope();
+		for (auto instr : steps)
+		{
+			my_rope->move(std::get<0>(instr), std::get<1>(instr));
+		}
 		return -1;
 	}
 
@@ -35,9 +51,47 @@ public:
 		return -1;
 	}
 
-	struct Rope
+	class Knot
 	{
+	public:
 		int x;
 		int y;
+		Knot(int x, int y)
+		{
+			this->x = x;
+			this->y = y;
+		}
 	};
+
+	class Rope
+	{
+	private:
+		Knot *head;
+		Knot *tail;
+	public:
+		Rope()
+		{
+			head = new Knot(0, 0);
+			tail = new Knot(0, 0);
+		}
+
+		void move(char dir, int n)
+		{
+			int x;
+			int y;
+			std::tie (x, y) = dir_offset.at(dir);
+			while (n-- > 0)
+			{
+				head->x += x;
+				head->y += y;
+				tailFollow();
+			}
+		}
+
+		void tailFollow()
+		{
+
+		}
+	};
+	
 };
