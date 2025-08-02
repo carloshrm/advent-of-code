d56a07f25d4c7badd8ae0cacacb015057073e7c8
carloshrm <carloshrmoraes@gmail.com>
2025-08-02 19:31:58 +0900

    day8 solution

--- 2015/day8.c
+++ 2015/day8.c
@@ -3,6 +3,48 @@
 #include <stdlib.h>
 #include <string.h>
 
+int count_original_characters(char *current_string, size_t len)
+{
+    int characters = 0;
+    for (size_t i = 1; i < len - 1; i++)
+    {
+        if (current_string[i] == '\\')
+        {
+            if (current_string[i + 1] == '\\' || current_string[i + 1] == '"')
+            {
+                i++;
+                characters++;
+            }
+            else if (current_string[i + 1] == 'x')
+            {
+                if (isxdigit(current_string[i + 2]) && isxdigit(current_string[i + 3]))
+                {
+                    i += 3;
+                    characters++;
+                }
+                else
+                    i += 2;
+            }
+        }
+        else
+            characters++;
+    }
+
+    return characters;
+}
+
+int count_encoded_characters(char *current_string, size_t len)
+{
+    int extra_encoded = 4;
+    for (size_t i = 1; i < len - 1; i++)
+    {
+        if (current_string[i] == '\\' || current_string[i] == '"')
+            extra_encoded++;
+    }
+
+    return extra_encoded;
+}
+
 int main()
 {
     FILE *file = fopen("./inputs/day8.txt", "r");
@@ -15,38 +57,14 @@ int main()
         size_t len = strlen(input_ln) - 1;
         total_code += len;
 
-        int characters = 0;
-        for (size_t i = 0; i < len; i++)
-        {
-            if (input_ln[i] == '"')
-                continue;
+        // int characters = count_original_characters(input_ln, len);
+        int characters = count_encoded_characters(input_ln, len) + len;
 
-            if (input_ln[i] == '\\')
-            {
-                if (input_ln[i + 1] == '\\' || input_ln[i + 1] == '"')
-                {
-                    i++;
-                    characters++;
-                }
-                else if (input_ln[i + 1] == 'x')
-                {
-                    if (isxdigit(input_ln[i + 2]) && isxdigit(input_ln[i + 3]))
-                    {
-                        i += 3;
-                        characters++;
-                    }
-                    else
-                        i += 2;
-                }
-            }
-            else
-                characters++;
-        }
-        // printf("\n string has %d code and %d chars", len, characters);
+        printf("\n string has %d code and %d chars", len, characters);
         total_char += characters;
     }
     fclose(file);
 
-    printf(" ::> %d code, %d chars for -- %d", total_code, total_char, total_code - total_char);
+    printf("\n ::> %d code, %d chars for -- %d", total_code, total_char, total_char - total_code);
     return 0;
 }
